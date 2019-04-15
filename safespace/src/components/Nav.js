e410d42a8484e9598d137bb4536dd207d09047fd
AJLambda <apj2277@gmail.com>
2019-04-15 15:30:31 -0700

    styled Nav

--- safespace/src/components/Nav.js
+++ safespace/src/components/Nav.js
@@ -3,17 +3,38 @@ import { NavLink } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <ul>
-      <li>
-        <NavLink to="/login">Login</NavLink>
-      </li>
-      <li>
-        <NavLink to="/signup">Sign Up</NavLink>
-      </li>
-      <li>
-        <NavLink to="/messagelist">My Messages</NavLink>
-      </li>
-    </ul>
+    <nav className="navbar navbar-inverse">
+      <div className="container-fluid">
+        <div className="navbar-header">
+          <a className="navbar-brand" href="#">
+            Safe Space
+          </a>
+        </div>
+        <ul className="nav navbar-nav">
+          <li className="active">
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <a href="#">My Profile</a>
+          </li>
+          <li>
+            <NavLink to="/messagelist">My Messages</NavLink>
+          </li>
+        </ul>
+        <ul className="nav navbar-nav navbar-right">
+          <li>
+            <NavLink to="/signup">
+              <span className="glyphicon glyphicon-user" /> Sign Up{" "}
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/login">
+              <span className="glyphicon glyphicon-log-in" /> Login{" "}
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
