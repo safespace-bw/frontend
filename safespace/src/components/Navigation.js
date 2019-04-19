3a7ef95bcc9d638e2d59a65bc8ac2252cc3e12cd
AJLambda <apj2277@gmail.com>
2019-04-18 17:16:12 -0700

    fixed logout i think

--- safespace/src/components/Navigation.js
+++ safespace/src/components/Navigation.js
@@ -25,12 +25,14 @@ const Navigation = props => {
             <a href="#">My Profile</a>
           </li> */}
           <li>
-            <NavLink to="/messagelist">My Messages</NavLink>
+            <NavLink to="/messagelist">
+              My Messages <i class="fas fa-sms fa-lg" />
+            </NavLink>
           </li>
         </ul>
         <ul className="nav navbar-nav navbar-right">
           <li>
-            <NavLink onClick={props.logOut}>
+            <NavLink exact to="/" onClick={props.logOut}>
               <i className="fas fa-sign-out-alt" /> Logout
             </NavLink>
           </li>
